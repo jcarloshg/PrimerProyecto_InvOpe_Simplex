b6dddcd1997d547e5fee09fa40761104ce9456e9
jcarloshg <carlosj12336@gmail.com>
2021-02-17 23:27:20 -0600

    ADD import numpy

---
+++ simplex.py
@@ -0,0 +1,13 @@
+# PRIMER PROJECTO [ SIMPLEX ]
+# Investigación Operaciones, Verano 2021
+
+# Alejandra Lizbeth Flores García
+# Luis David Hernández Reyes
+# Jose Carlos Huerta García
+
+
+from numpy import array
+
+tablo = array([1,2,3,4,5,6,7,7])
+
+print(tablo)
